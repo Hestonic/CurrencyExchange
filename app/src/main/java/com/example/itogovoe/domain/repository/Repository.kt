7122695bb24f9d693d5ec315e99595463abad1cd
danii <daniilgeroldovich@gmail.com>
2022-04-10 10:03:36 +0300

    modernized Room repository return data

--- app/src/main/java/com/example/itogovoe/domain/repository/Repository.kt
+++ app/src/main/java/com/example/itogovoe/domain/repository/Repository.kt
@@ -1,58 +1,67 @@
 package com.example.itogovoe.domain.repository
 
 import android.util.Log
+import com.example.itogovoe.data.api.CurrencyResponse
 import com.example.itogovoe.data.source.LocalDataSource
 import com.example.itogovoe.data.source.RemoteDataSource
+import com.example.itogovoe.data.source.local_source.entities.InfoEntity
 import com.example.itogovoe.domain.mapper.CurrencyDtoMapper
 import com.example.itogovoe.domain.model.Currencies
+import retrofit2.Response
 import java.time.LocalDateTime
-import java.time.Period
 import java.time.temporal.ChronoUnit
-import java.util.*
 
 class Repository(
     private val localDataSource: LocalDataSource,
     private val remoteDataSource: RemoteDataSource
 ) {
 
+    private lateinit var response: Response<CurrencyResponse>
+    private lateinit var domainModel: Currencies
+    private lateinit var infoEntity: InfoEntity
+
     suspend fun getCurrencies(): Currencies? {
         try {
-            // Получаем данные из сети
+            // Получаем данные из локального хранилища
             val localCurrencies = localDataSource.readAllCurrencies()
-            val response = remoteDataSource.getCurrencies()
-            val domainModel = CurrencyDtoMapper.mapResponseToDomainModel(response)
             // Если БД пустая - берём данные из сети и загружаем в БД
             if (localCurrencies.isEmpty()) {
+                response = remoteDataSource.getCurrencies()
+                domainModel = CurrencyDtoMapper.mapResponseToDomainModel(response)
                 val currenciesEntity =
                     CurrencyDtoMapper.mapDomainModelToCurrenciesEntity(domainModel)
                 currenciesEntity?.forEach { localDataSource.addCurrencyItem(it) }
-                val infoEntity = CurrencyDtoMapper.mapDomainModelToInfoEntity(domainModel)
+                infoEntity = CurrencyDtoMapper.mapDomainModelToInfoEntity(domainModel)
                 localDataSource.addInfo(infoEntity)
                 Log.d("repository_return_tag", "добавляем данные в БД")
             } else if (localCurrencies.isNotEmpty()) {
-                val infoEntity = localDataSource.readAllInfo()
+                infoEntity = localDataSource.readAllInfo()
                 val dateNow = LocalDateTime.now()
                 val minutes = ChronoUnit.MINUTES.between(infoEntity.lastUploadDate, dateNow)
                 Log.d("difference_date", minutes.toString())
                 // Проверяем данные на "старость"
                 return if (minutes < 5) {
-                    Log.d("repository_return_tag", "вернули старые данные")
-                    val currenciesEntity =
-                        CurrencyDtoMapper.mapDomainModelToCurrenciesEntity(domainModel)
-                    CurrencyDtoMapper.mapCurrenciesEntityToDomainModel(currenciesEntity, infoEntity)
+                    Log.d("repository_return_tag", "вернули локальные данные")
+                    CurrencyDtoMapper.mapCurrenciesEntityToDomainModel(localCurrencies, infoEntity)
                 } else {
-                    Log.d("repository_return_tag", "вернули новые данные")
+                    Log.d("repository_return_tag", "вернули данные из сети")
+                    response = remoteDataSource.getCurrencies()
+                    domainModel = CurrencyDtoMapper.mapResponseToDomainModel(response)
+                    // удалили старые данные
                     localDataSource.deleteAllCurrencies()
                     localDataSource.deleteAllInfo()
-                    val infoEntity = CurrencyDtoMapper.mapDomainModelToInfoEntity(domainModel)
+                    // добавили новые
+                    infoEntity = CurrencyDtoMapper.mapDomainModelToInfoEntity(domainModel)
                     val currenciesEntity =
                         CurrencyDtoMapper.mapDomainModelToCurrenciesEntity(domainModel)
                     currenciesEntity?.forEach { localDataSource.addCurrencyItem(it) }
                     localDataSource.addInfo(infoEntity)
+                    // вернули актуальные данные
                     domainModel
                 }
             }
-            return domainModel
+            response = remoteDataSource.getCurrencies()
+            return CurrencyDtoMapper.mapResponseToDomainModel(response)
         } catch (e: Exception) {
             e.printStackTrace()
             return null
